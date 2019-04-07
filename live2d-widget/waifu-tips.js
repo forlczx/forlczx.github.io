3535b4c9a2d3273f3b05a99d474d044e0f8b7f57
janeswebsite <3377048848@qq.com>
2019-04-07 10:38:10 +0800

    Site updated: 2019-04-07 10:38:03

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -34,7 +34,7 @@ function initWidget(waifuPath, apiPath) {
 		window.Live2D.captureFrame = true;
 	});
 	$("#waifu-tool .fa-info-circle").click(function() {
-		window.open("https://github.com/stevenjoezhang/live2d-widget");
+		window.open("https://janeswebsite.github.io/");
 	});
 	$("#waifu-tool .fa-times").click(function() {
 		localStorage.setItem("waifu-display", new Date().getTime());
